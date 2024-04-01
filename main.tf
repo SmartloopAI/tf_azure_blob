d6f1a4e94d90109b59ce211cfe61a7eae6046c64
Mehfuz Hossain <mehfuz@gmail.com>
2024-04-01 16:32:21 -0700

    random number in storage account

--- main.tf
+++ main.tf
@@ -4,13 +4,22 @@ locals {
   }
 }
 
+resource "random_integer" "num" {
+  min = 1
+  max = 50000
+  keepers = {
+    # Generate a new integer each time we switch to a new listener ARN
+    listener_arn = var.storage_account_name
+  }
+}
+
 resource "azurerm_storage_account" "static_storage" {
-  name                     = "${var.resource_name}${var.storage_account_name}"
-  resource_group_name      = data.azurerm_resource_group.this.name
-  location                 = data.azurerm_resource_group.this.location
-  account_kind             = "StorageV2"
-  account_tier             = "Standard"
-  account_replication_type = "GRS"
+  name                      = "${var.storage_account_name}${random_integer.num.result}"
+  resource_group_name       = data.azurerm_resource_group.this.name
+  location                  = data.azurerm_resource_group.this.location
+  account_kind              = "StorageV2"
+  account_tier              = "Standard"
+  account_replication_type  = "GRS"
   enable_https_traffic_only = true
 
   static_website {
